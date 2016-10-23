45bf461ed6a63d90f17bbfb373483655a6c799b6
Zachary Townsend <zach@verbbrands.com>
2016-10-23 21:24:30 +0100

    Some Routing and added some images

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET test page */
+router.get('/test', function(req, res, next) {
+	res.send('Hello World!');
+});
+
 module.exports = router;
