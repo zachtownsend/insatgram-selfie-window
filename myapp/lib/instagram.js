e46ce38618b2e4143744819626aadb29d0f84c22
Zachary Townsend <zach@verbbrands.com>
2016-10-25 23:33:25 +0100

    changed to Tilly
    learned that Sandbox mode only allows you to use user’s tags

--- myapp/lib/instagram.js
+++ myapp/lib/instagram.js
@@ -1,8 +1,21 @@
 var accessToken = '1714767474.4f5e0e7.fd2c70bf22404ab8ad65553941d396b0';
-var Instagram = require('instagram-node').instagram();
+var InstagramAPI = require('instagram-api');
 var InstagramAPI = new InstagramAPI(accessToken);
-Instagram.use({access_token: accessToken});
+var block = false;
 
-Instagram.tag_media_recent('selfie', {count: 50}, function(){
-	console.log(err, medias, pagination, remaining, limit);
-});
+function getMedia(){
+	if (block) {
+		return false;
+	}
+	InstagramAPI.getMediasByTag('tilly', {count: 20 }).then(function(result){
+		console.log(result.data);
+	}, function(err){
+		console.log(err);
+	});
+	block = true;
+	setTimeout(function(){
+		block = false;
+	}, (500/60)*1000);
+}
+
+getMedia();
