02cb6f1c0eb20f19dc9e8ded9583d7414ee0c426
Zachary Townsend <zach@verbbrands.com>
2016-10-25 22:09:51 +0100

    Instagram API node.js

--- myapp/lib/instagram.js
+++ myapp/lib/instagram.js
@@ -1,8 +1,14 @@
-var accessToken = '1714767474.1677ed0.8f3526957e4b46e1aeb547044a2a23d2';
+var accessToken = '1714767474.4f5e0e7.fd2c70bf22404ab8ad65553941d396b0';
 var InstagramAPI = require('instagram-api');
 var InstagramAPI = new InstagramAPI(accessToken);
-alert('test');
-instagramAPI.userSelf().then(function(result){
+// alert('test');
+// InstagramAPI.userSelf().then(function(result){
+// 	console.log(result);
+// }, function(err){
+// 	console.log(err);
+// });
+
+InstagramAPI.getMediasByTag('selfie', {count: 50}).then(function(result){
 	console.log(result);
 }, function(err){
 	console.log(err);
